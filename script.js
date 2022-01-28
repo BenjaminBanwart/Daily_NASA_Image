5f5c73f39f5c86c508380e25727751ee9bd66605
Benjamin Banwart <benjibanwart@gmail.com>
2022-01-28 15:54:21 -0600

    Created project on local machine, may update

---
+++ script.js
@@ -0,0 +1,18 @@
+//request data from NASA API 
+async function getData() {
+    var key = config.API_KEY;
+    let response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${key}`);
+    let data = await response.json()
+    console.log(data)
+    createPage(data)
+}
+
+//fill html elements with data
+function createPage(data) {
+    document.getElementById('title').innerHTML += data.title;
+    document.getElementById('daily-image').src = data.hdurl;
+    document.getElementById('copyright').innerHTML += "By "+data.copyright;
+    document.getElementById('explanation').innerHTML += data.explanation;
+}
+
+getData()
